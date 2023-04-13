df149cb08fcca1d90f79b40d37a1bcd1bdff6ac6
Devesh Udhlani <80637738+646divi@users.noreply.github.com>
2023-04-13 15:27:00 +0530

    Delete text_to_speech.py

--- text_to_speech.py
+++
@@ -1,9 +0,0 @@
-import pyttsx3 as p
-
-engine = p.init()
-
-# engine.save_to_file('You selected apps and games','apps_and_games.mp3')
-# engine.runAndWait()
-engine.save_to_file('You selected Paid games','Paid_and_Mod_games.mp3')
-engine.runAndWait()
-engine.stop()
